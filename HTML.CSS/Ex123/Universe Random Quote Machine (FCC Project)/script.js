6cb57d9dd9b1fab0faa5d8ae3c68a538ae36c21e
jsatheesh568 <jsatheesh568@gmail.com>
2024-07-08 07:52:12 +0530

    URQM

---
+++ HTML.CSS/Ex123/Universe Random Quote Machine (FCC Project)/script.js
@@ -0,0 +1,456 @@
+var quoteObject={
+  "quotes":[
+{
+"text":"Yeah, yeah, don't forget: reckless, vulgar, loud-mouthed. That's just what makes me so awesome!",
+"character":"Amethyst"
+},
+{
+"text":"In the ring, nobody can tell me what to do! And if they try, I HIT 'EM IN THE FACE WITH A CHAIR!",
+"character":"Amethyst"
+},
+{
+"text":"Listen, Steven. You can't let anyone make you feel like garbage.",
+"character":"Amethyst"
+},
+{
+"text":"Garnet, do something.",
+"character":"Amethyst"
+},
+{
+"text":"Humans should just stop wearing clothes... It'd be a lot funnier.",
+"character":"Amethyst"
+},
+{
+"text":"If you're trying to flush yourself down the toilet, it won't work. Trust me, I've tried.",
+"character":"Amethyst"
+},
+{
+"text":"The most exciting thing in my life is tennis practice. Forehand! Backhand! Overhead death strike!",
+"character":"Connie"
+},
+{
+"text":"Man, that's really stuck on good. I guess we'll have to chop it off.",
+"character":"Connie"
+},
+{
+"text":"Steven, let's just hop on a bus and live somewhere else without telling anyone!",
+"character":"Connie"
+},
+{
+"text":"You don't need powers to be here with me.",
+"character":"Connie"
+},
+{
+"text":"Run back to your masters, tell them we're not afraid of your kind!",
+"character":"Connie"
+},
+{
+"text":"Hey! Don't mess with his funky flow!",
+"character":"Garnet"
+},
+{
+"text":"I... I thought violence would be the answer.",
+"character":"Garnet"
+},
+{
+"text":"Now it has all the power of a breakfast. We have to destroy it.",
+"character":"Garnet",
+     
+},
+{
+"text":"We don't need to be careful. We just need to be huge.",
+"character":"Garnet"
+
+},
+{
+"text":"No dinner for 1,000 years.",
+"character":"Garnet"
+},
+{
+"text":"All comedy is derived from fear.",
+"character":"Garnet"
+},
+{
+"text":"Tiny hands. My only weakness.",
+"character":"Garnet"
+
+},
+{
+"text":"That's a lie. Your middle name is cutie pie.",
+"character":"Garnet"
+},
+{
+"text":"It's okay, Steven, I'm never alone.",
+"character":"Garnet"
+},
+{
+"text":"Garnet, master of comedy.",
+"character":"Garnet"
+},
+{
+"text":"When two Gems combine, it creates something greater than the sum of their parts. That's why I'm so great.",
+"character":"Garnet"
+},
+{
+"text":"Get open, get honest, invent yourselves together. That's fusion.",
+"character":"Garnet"
+
+},
+{
+"text":"Alright no more fighting. Let's just have a civil conversation. ...Okay, let's kick her butt.",
+"character":"Garnet"
+},
+{
+"text":"Alright, I chased away those cows. Let's get to work.",
+"character":"Garnet"
+},
+{
+"text":"I blame the cows.",
+"character":"Garnet"
+},
+{
+"text":"Don't worry Peridot, it's our sworn duty to protect anything that calls this planet home, and that includes clods like you.",
+"character":"Garnet"
+},
+
+{
+"text":"If every pork chop were perfect, we wouldn't have hot dogs.",
+"character":"Greg",
+  },
+{
+"text":"Guys, I just had the best idea for an album cover.",
+"character":"Greg"
+},
+{
+"text":"Now, remember, Steven, if you run into any trouble out there, you can always bail. There's never any shame in bailing.",
+"character":"Greg"
+},
+{
+"text":"A responsible parent doesn't let two kids strand themselves in a van in the middle of nowhere. ... A responsible parent escorts those kids through wind, sleet, and snow to avoid making the other parent angry!",
+"character":"Greg"
+},
+{
+"text":"Yeah, he's just an amigo from way back when... He's dead to me.",
+"character":"Greg"
+},
+{
+"text":"Son, there will come a time in your life when you'll learn to accept all pizza.",
+"character":"Greg"
+},
+{
+"text":"I'm going to see a man about a tunnel brush... An internet man. If I'm not back in an hour, call the police.",
+"character":"Greg"
+},
+{
+"text":"No such thing as a good war, kiddo.",
+"character":"Greg"
+},
+{
+  "text":"I just want to go home.",
+  "character":"Lapis"
+},
+
+{
+"text":"I'm done being everyone's prisoner! Now you're MY prisoner! And I'm never letting you go.",
+"character":"Lapis"
+},
+
+{
+"text":"Let's stay on this miserable planet, together!",
+"character":"Lapis"
+},
+{
+"text":"Did you even wonder who I used to be?",
+"character":"Lapis"
+},
+{
+"text":"It's funny. For all this time I spent on Earth, I barely saw any of it.",
+"character":"Lapis"
+},
+{
+"text":"This plan sucks.",
+"character":"Lapis"
+},
+{
+"text":"This just goes to show that you should always listen to me, and never listen to Amethyst.",
+"character":"Pearl"
+},
+{
+"text":"Some of us are trying to protect humanity!",
+"character":"Pearl"
+},
+{
+"text":"Humans find such fascinating ways to waste their time.",
+"character":"Pearl"
+},
+{
+"text":"Well, that's perfect, because I don't want to go with grammatically incorrect people anyway.",
+"character":"Pearl"
+},
+{
+"text":"You have to feel like yourself! Sweet and considerate, and only occasionally obnoxious!",
+"character":"Pearl"
+},
+{
+"text":"There are... different ways of being strong.",
+"character":"Pearl"
+},
+{
+"text":"Ugh... I hate fighting me!",
+"character":"Pearl"
+},
+{
+"text":"I like to watch you sleep sometimes. And by sometimes, I mean often.",
+"character":"Pearl"
+},
+{
+"text":"Everything I ever did, I did for her. And now she's gone. But I'm still here.",
+"character":"Pearl"
+},
+{
+"text":"You always say you'll be the crocodile, but you never commit!",
+"character":"Pearl"
+},
+{
+"text":"I don't understand your human relationships. So, um... goodbye!",
+"character":"Pearl"
+},
+{
+"text":"I am a gem of many talents. A dedication to fact is just one of them.",
+"character":"Pearl"
+},
+{
+"text":"I don't get tired, I get results.",
+"character":"Pearl"
+},
+{
+"text":"Okay, I realize that was a bit of a fiasco.",
+"character":"Pearl"
+},
+{
+"text":"That's right, you can feel horrible all you want back at the Temple.",
+"character":"Pearl"
+},
+{
+"text":"There appears to be an infestation of 'Stevens' in the Kindergarten.",
+"character":"Peridot"
+},
+{
+"text":"'Go to Earth,' they said. 'It'll be easy,' they said.",
+"character":"Peridot"
+},
+{
+"text":"Don't touch that! You clods don't know what you're doing!",
+"character":"Peridot"
+},
+{
+"text":"Oh my stars, you're gonna harvest me?",
+"character":"Peridot"
+},
+{
+"text":"Get your touch-stumps off me, you Steven!",
+"character":"Peridot"
+},
+{
+"text":"Look! Over there! Another planet to betray!",
+"character":"Peridot"
+},
+{
+"text":"Yes! Feel my unbridled rage!",
+"character":"Peridot"
+},
+{
+"text":"You smashed me into a limbless cloud, you trapped me in your bubble dungeon, and you called me cute!",
+"character":"Peridot"
+},
+{
+"text":"What a great souvenir of that other time you assaulted me.",
+"character":"Peridot"
+},
+{
+"text":"Negative. I wasn't lucky enough to be around for that. But I read over a few hundred years of reports.",
+"character":"Peridot"
+},
+{
+"text":"B-but I won! What about the rules?!",
+"character":"Peridot"
+},
+{
+"text":"I'm still learning. I hope you understand. I want to understand. I'm sorry.",
+"character":"Peridot"
+},
+{
+"text":"Picture? This isn't just a picture Steven! It's a complex chart cataloging the compatible characteristics between campers.",
+"character":"Peridot"
+},
+{
+"text":"Am I the only one who understands the meaning of teamwork?!",
+"character":"Peridot"
+},
+{
+"text":"Every moment you love being yourself, that's me, loving you and loving being you. Because you're going to be something extraordinary. You're going to be a human being.",
+"character":"Rose"
+},
+{
+"text":"I love humans! You're all so funny!",
+"character":"Rose"
+},
+{
+"text":"I'm... not a real person. I thought... haven't we... is this not how it works?",
+"character":"Rose"
+},
+{
+"text":"Who cares about how I feel? How you feel is bound to be much more interesting.",
+"character":"Rose"
+},
+{
+"text":"No more questions. Don't ever question this. You already are the answer.",
+"character":"Rose"
+},
+{
+"text":"It's fusion, Sapphire! What's more personal to us than fusion?",
+"character":"Ruby"
+},
+{
+"text":"I'm an eternal flame, baby!",
+"character":"Ruby"
+},
+{
+"text":"Well, Garnet's not here!",
+"character":"Ruby"
+},
+{
+"text":"You know what's nice about being split up? I get to look at you.",
+"character":"Ruby"
+},
+{
+"text":"Don't worry, you can look at me when you're running for home.",
+"character":"Ruby"
+},
+{
+"text":"I'm... fine.",
+"character":"Sapphire"
+},
+{
+"text":"Of course not. Can't you see I'm completely engulfed in rage?",
+"character":"Sapphire"
+},
+{
+"text":"Such is fate.",
+"character":"Sapphire"
+},
+{
+"text":"I can't just start listening now, I'd be lost! Just like my pants...",
+"character":"Steven"
+},
+{
+"text":"As soon as I bared my butt, I knew he'd crack.",
+"character":"Steven"
+},
+{
+"text":"So, when you fuse, do you turn into a GIANT giant woman, or just a regular sized giant woman?",
+"character":"Steven"
+},
+{
+"text":"Try to act like a rich duck.",
+"character":"Steven"
+},
+{
+"text":"None of you are the handsome one! I'm the handsome one!",
+"character":"Steven"
+},
+{
+"text":"I'm gonna bring the ocean back... or get really thirsty trying.",
+"character":"Steven"
+},
+{
+"text":"Garnet learned a valuable lesson that day: that true strength is about more than just how many mountains you can punch in half.",
+"character":"Steven"
+},
+{
+"text":"Pizza rain! ...But no pizza clouds...",
+"character":"Steven"
+},
+{
+"text":"Wow, I want four legs too!",
+"character":"Steven"
+},
+{
+"text":"Safety is no punchline, Pearl.",
+"character":"Steven"
+},
+{
+"text":"Sleep is a curse, and yet... a curse I need to live.",
+"character":"Steven"
+},
+{
+"text":"Square pizza!? What's wrong with this crazy state!?",
+"character":"Steven"
+},
+{
+"text":"I can't eat such a perfect sandwich all on its own! It needs a side! ...But what?",
+"character":"Steven"
+},
+{
+"text":"Have a great weekend! ...I mean... I hope her weekend is not so great.",
+"character":"Steven"
+},
+{
+"text":"Awwww! You're like... an angry little slice of pie!",
+"character":"Steven"
+},
+{
+"text":"Wait! I have a better idea that doesn't involve destroying the house!",
+"character":"Steven"
+},
+{
+"text":"Sweet, two doughnuts! One for me and one... for... uh, me.",
+"character":"Stevonnie"
+},
+{
+"text":"It's Stevonnie- I am not your baby.",
+"character":"Stevonnie"
+}
+],
+ "portraits":{
+	"Amethyst":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/amethyst-portrait.jpg",
+	"Connie":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/connie-portrait.jpg",
+	"Garnet":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/garnet-portrait.jpg",
+	"Greg":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/greg-portrait.jpg",
+	"Lapis":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/lapis-portrait.jpg",
+	"Pearl":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/pearl-portrait.jpg",
+	"Peridot":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/peridot-portrait.jpg",
+	"Rose":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/rose-portrait.jpg",
+	"Ruby":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/ruby-portrait.jpg",
+	"Sapphire":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/sapphire-portrait.jpg",
+	"Steven":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/steven-portrait.jpg",
+	"Stevonnie":"https://raw.githubusercontent.com/ameliavoncat/su-quote-generator/portraits/stevonnie-portrait.jpg"
+}	
+};
+
+  $(document).ready(function(){
+    //INITIALIZE//
+  var quote=0;
+    //Function to randomly select quotes//
+  function newQuote(){
+    //While loop prevents repeating the same quote//
+    var previousQuote=quote;
+   while(previousQuote==quote){ 
+     quote=quoteObject.quotes[Math.floor(Math.random()*quoteObject.quotes.length)];
+   }
+    }
+  //Function updates html with the new quote//
+  function assignQuote(){
+    $("#quote-body").html(quote.text);
+    $("#quote-attribution").html(quote.character);
+    $("#twitter-button").attr("href","https://twitter.com/intent/tweet?text="+quote.text+"%20-"+quote.character);
+    $("#character-image").attr("src",quoteObject.portraits[quote.character]);
+    $("#quote-box").attr("class",quote.character);
+     }
+  //Button press calls the functions//
+  $("#quote-button").on("click", function(){
+      newQuote();
+      assignQuote();      
+    });
+    //Document.ready ends below//
+  });
