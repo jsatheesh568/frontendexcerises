6089b17dd6c03cd27642ee2964fe121410c46441
jsatheesh568 <jsatheesh568@gmail.com>
2024-09-26 07:11:09 +0530

    dark mode

---
+++ HTML.CSS/Ex195/Light Mode _ Dark Mode Toggle/script.js
@@ -0,0 +1,4 @@
+const checkbox = document.querySelector("#checkbox");
+const mainBody = document.querySelector(".mainBody");
+
+checkbox.addEventListener("click", () => mainBody.classList.toggle("darkBg"));
